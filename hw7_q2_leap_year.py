d5ba555c93d93860215f33e4fd37ad7e80ca19c2
kimkw88 <kimkw@oregonstate.edu>
2021-03-02 23:26:39 -0800

    Wrote first test
    Two files have been created for calculating the leap year and demonstrating pass and fail conditions.

---
+++ hw7_q2_leap_year.py
@@ -0,0 +1,17 @@
+# CS362_Winter21
+# HW7
+# Program: hw7_q2_leap_year.py
+# Author: Kwanghyuk Kim
+
+###########################################################################
+# Program Description
+# : An application that calculates whether the year is a leap year or not.
+# From homework 1, rewrite the leap year program using the test-first approach.
+#
+# To compile and run this code, please just enter the command as below: 
+#
+# python3 hw7_q2_leap_year.py
+###########################################################################
+
+def is_leap(year):
+    pass
---
+++ hw7_tests_q2_leap_year.py
@@ -0,0 +1,27 @@
+# CS362_Winter21
+# HW7
+# Program: hw7_tests_q2_leap_year.py
+# Author: Kwanghyuk Kim
+
+###########################################################################
+# Program Description
+# : An application that demonstrates pass and fail conditions for the program.
+#
+# To compile and run this code, please just enter the command as below: 
+#
+# python3 hw7_tests_q2_leap_year.py
+###########################################################################
+
+import unittest
+import hw7_q2_leap_year as q2
+
+class LeapYearTests(unittest.TestCase):
+
+    def test_equal1(self):
+        self.assertEqual(q2.is_leap(2020), True)
+    def test_equal2(self):
+        self.assertEqual(q2.is_leap(2021), False)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
